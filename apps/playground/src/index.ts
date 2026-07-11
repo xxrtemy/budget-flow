52a3b5c7acfdadb9c5fbb62fd23ca5e0524d7542
xxrtemy <rebelgamechannel@gmail.com>
2026-07-11 20:54:17 +0300

    delete example

--- apps/playground/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { APP_NAME, type Currency } from "@budget-flow/shared";
-
-const defaultCurrency: Currency = "RUB";
-
-console.log(`Application: ${APP_NAME}`);
-console.log(`Default currency: ${defaultCurrency}`);
